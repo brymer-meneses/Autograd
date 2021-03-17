5e6467747222e4431d8e549b054ca67ad9a5fb17
Brymer-Meneses <meneses.brymer@gmail.com>
2021-03-17 13:58:35 +0800

    clean up the API

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,111 +1,26 @@
-use std::ops;
-use std::any::Any;
+// use std::ops;
 
-pub struct Variable {
-    pub value: f64,
-    pub requires_grad: bool, 
-    pub depends_on: Vec<(Variable, f64)>,
-    pub grad: f64,
+struct Variable<'a> {
+    pub value: f64, 
+    requires_grad: bool,
+    depends_on: Vec<(&'a Variable<'a> ,f64)>,
+    pub grads: f64,
+
 }
 
-// #[derive(Default)]
-impl Default for Variable {
-    fn default() -> Self {
-        Variable {
-            value: 0 as f64,
-            grad: 0 as f64,
-            requires_grad: false,
-            depends_on: Vec::new(),
-        }
-
-    }
+trait BasicOperations {
+    fn add(v1: &Variable, v2: &Variable) -> &Variable;
+    fn mul(v1: &Variable, v2: &Variable) -> &Variable;
 }
 
-
-impl Variable {
-    pub fn backwards(self) {
-    
-        fn compute_gradients(variable: Variable, previous_grad: f64) {
-            for dependency in variable.depends_on {
-                let mut next_variable = dependency.0;
-                let grad = dependency.1;
-
-                let this_variable_grads = previous_grad * grad;
-                next_variable.grad += this_variable_grads;
-
-                compute_gradients(next_variable, this_variable_grads);
-
-
-            }
-        }
-
-        compute_gradients(self, 1 as f64);
-    }
-
-    pub fn new(value: f64, requires_grad: bool) -> Variable {
-
-        let new_variable = Variable {
-            value: value, 
-            grad: 0.,
-            requires_grad: requires_grad, 
-            depends_on: Vec::new(),
-        };
-
-        return new_variable
-
-    }
-}
-
-impl ops::Add for Variable {
-    type Output = Variable;
-
-    fn add(self, v: Variable) -> Variable{
-        let result = self.value + v.value;
-        let requires_grad = self.requires_grad || v.requires_grad;
+impl BasicOperations for Variable {
+    fn add(v1: &Variable, v2: &Variable) -> &Variable {
+        let result = v1.value + v2.value;
+        let requires_grad = v1.requires_grad || v2.requires_grad;
         let mut dependency = Vec::new();
 
-        if self.requires_grad {
-            dependency.push((self, 1 as f64));
-        }
-
-        if v.requires_grad {
-            dependency.push((v, 1 as f64));
-        }
-
-        return Variable{
-            value: result, 
-            requires_grad: requires_grad,
-            depends_on: dependency,
-            grad: 0 as f64,
+        if v1.requires_grad {
+            dependency.append((v1, 1 as f64));
         }
     }
-}
-
-impl ops::Mul for Variable {
-    type Output = Variable;
-
-    fn mul(self, v: Variable) -> Variable{
-        let lhs_value = self.value;
-        let rhs_value = v.value;
-
-        let result = lhs_value * rhs_value;
-        let requires_grad = self.requires_grad || v.requires_grad;
-        let mut dependency = Vec::new();
-
-        if self.requires_grad {
-            dependency.push((self, lhs_value));
-        }
-
-        if v.requires_grad {
-            dependency.push((v, rhs_value));
-        }
-
-        return Variable{
-            value: result, 
-            requires_grad: requires_grad,
-            depends_on: dependency,
-            grad: 0 as f64,
-        }
-    }
-
 }
