4453bf126999c495d640ef1f7150e5b2c0a14205
Brymer-Meneses <meneses.brymer@gmail.com>
2021-03-21 00:48:21 +0800

    initial refactor of rust

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,66 +1,127 @@
-// use std::ops;
 
-struct Variable<'a> {
-    pub value: f64, 
-    requires_grad: bool,
-    depends_on: Vec<(&'a Variable<'a> ,f64)>,
-    pub grads: f64,
 
+#[allow(dead_code)]
+#[derive(Debug, PartialEq)]
+pub struct Variable<'a> {
+
+    // Stores the numerical value
+    pub value: f64,
+
+    // TODO add comment
+    pub requires_grad: bool,
+
+    // Stores the parent node/s that is/are required for backpropagation
+    pub parent_nodes: Vec<(&'a Variable<'a>, f64)>,
+
+    // Will be assigned a value after backpropagation
+    pub grad: f64,
 }
 
-trait BasicOperations<'a> {
-    fn add(v1: &'a Variable, v2: &'a Variable) -> &'a Variable<'a>;
-    fn mul(v1: &'a Variable, v2: &'a Variable) -> &'a Variable<'a>;
-}
 
-trait TrigonometricFunctions {
-    fn cos<'a> (v1: &'a Variable) -> &'a Variable<'a>;
-    fn sin<'a> (v1: &'a Variable) -> &'a Variable<'a>;
-}
+#[allow(dead_code)]
+pub fn _add<'a> (v1: &'a Variable, v2: &'a Variable) -> Variable<'a> {
+    let value = v1.value + v2.value;
+    let requires_grad = v1.requires_grad || v2.requires_grad;
+    let mut parent_nodes = Vec::new();
 
-impl<'a> BasicOperations for Variable<'a> {
-    fn add(v1: &'a Variable, v2: &'a Variable) -> &'a Variable<'a> {
-
-        let result = v1.value + v2.value;
-        let requires_grad = v1.requires_grad || v2.requires_grad;
-        let mut dependency = Vec::new();
-
-        if v1.requires_grad {
-            dependency.push((v1, 1 as f64));
-        } 
-        
-        if v2.requires_grad {
-            dependency.push((v2, 1 as f64));
-        }
-
-        return &Variable {
-            value: result,
-            requires_grad: requires_grad,
-            depends_on: dependency,
-            grads: 0 as f64,
-        }
+    if v1.requires_grad {
+        let grad = 1.0;
+        parent_nodes.push((v1, grad));
     }
 
-    fn mul(v1: &'a Variable, v2: &'a Variable) -> &'a Variable<'a> {
-        
-        let result = v1.value * v2.value;
-        let requires_grad = v1.requires_grad || v2.requires_grad;
-        let mut dependency = Vec::new();
-
-        if v1.requires_grad {
-            dependency.push((v1, v2.value));
-        } 
-        
-        if v2.requires_grad {
-            dependency.push((v2, v1.value));
-        }
-
-        return &Variable {
-            value: result,
-            requires_grad: requires_grad,
-            depends_on: dependency,
-            grads: 0 as f64,
-        }
+    if v2.requires_grad {
+        let grad = 1.0;
+        parent_nodes.push((v2, grad));
     }
 
+    return Variable {
+        value,
+        requires_grad,
+        parent_nodes,
+        grad: 0.0,
+    }
+}
+
+#[allow(dead_code)]
+pub fn _mul<'a> (v1: &'a Variable, v2: &'a Variable) -> Variable<'a> {
+    let value = v1.value * v2.value;
+    let requires_grad = v1.requires_grad || v2.requires_grad;
+    let mut parent_nodes = Vec::new();
+
+
+    if v1.requires_grad {
+        let grad = v2.value;
+        parent_nodes.push((v1, grad));
+    }
+
+    if v2.requires_grad {
+        let grad= v1.value;
+        parent_nodes.push((v2, grad));
+    }
+
+    return Variable {
+        value,
+        requires_grad,
+        parent_nodes,
+        grad: 0.0,
+    }
+}
+
+trait AutoGrad {
+    fn backwards(&mut self); 
+    // fn zero_grad(&mut self) -> Self;
+}
+
+impl<'a> Variable<'a> {
+    fn backwards(mut self) {
+        fn cal_gradients<'a>(v: &mut Variable<'a>, parent_grad: f64) {
+            for (variable, grad) in &mut v.parent_nodes {
+
+                if variable.requires_grad {
+                    let _grad = grad.clone();
+                    let next_var_grad = parent_grad * _grad;
+                    variable.grad += next_var_grad;
+                } else {
+                    continue 
+                }
+            }
+        }
+        cal_gradients(&mut self, 1.0);
+    }
+}
+
+#[macro_export]
+macro_rules! add {
+    ($v1:ident, $v2:ident) => {
+        {
+            let result = autograd::_add(&$v1, &$v2);
+            result
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! mul {
+    ($v1:ident, $v2:ident) => {
+        {
+            let result = autograd::_mul(&$v1, &$v2);
+            result
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! new {
+    ($value:expr, $requires_grad:expr) => {
+        {
+            let value = $value as f64;
+            autograd::Variable {
+                value, 
+                requires_grad: $requires_grad, 
+                grad: 0.0,
+                parent_nodes: Vec::new(),
+            }
+        }
+        
+    };
 }
--- rust/tests/test.rs
+++ rust/tests/test.rs
@@ -1,30 +1,67 @@
-use autograd::Variable;
+use autograd::new; 
+use autograd::add;
+use autograd::mul;
+// use autograd::mul;
+
 
 #[test]
-fn it_works() {
-    let a = Variable{
-        value: 2 as f64,
-        grad: 0 as f64,
-        requires_grad: false,
-        depends_on: Vec::new(), 
-    };
+fn variable() {
+    let a = new!(2, true);
 
-    let b = Variable{
-        value: 3 as f64,
-        grad: 0 as f64,
-        requires_grad: false,
-        depends_on: Vec::new(), 
-    };
-
-
-    let c = a.clone() + b;
-    c.backwards();
-
-    // println!("{}", a.grad);
-    
-    assert_eq!(1 as f64, a.grad)
-
+    assert_eq!(a.value, 2.0);
+    assert_eq!(a.requires_grad, true);
+    assert_eq!(a.grad, 0.0);
+    assert_eq!(a.parent_nodes, Vec::new());
 }
 
+#[test]
+fn add_single_grad() {
+    let a = new!(2, true);
+    let b = new!(2, false);
 
+    let c = add!(a, b);
+    
+    assert_eq!(c.value, 4.0);
+    assert_eq!(c.grad, 0.0);
+    assert_eq!(c.requires_grad, true);
+    assert_eq!(c.parent_nodes, vec![(&a, 1.0)]);
+}
 
+#[test]
+fn add_multiple_grad() {
+    let a = new!(5, true);
+    let b= new!(2, true);
+
+    let c = add!(a, b);
+
+    assert_eq!(c.value, 7.0);
+    assert_eq!(c.grad, 0.0);
+    assert_eq!(c.requires_grad, true);
+    assert_eq!(c.parent_nodes, vec![(&a, 1.0), (&b, 1.0)])
+}
+
+#[test]
+fn mul_single_grad() {
+    let a = new!(2, true);
+    let b = new!(2, false);
+
+    let c = mul!(a, b);
+    
+    assert_eq!(c.value, 4.0);
+    assert_eq!(c.grad, 0.0);
+    assert_eq!(c.requires_grad, true);
+    assert_eq!(c.parent_nodes, vec![(&a, b.value)]);
+}
+
+#[test]
+fn mul_multiple_grad() {
+    let a = new!(5, true);
+    let b= new!(2, true);
+
+    let c = mul!(a, b);
+
+    assert_eq!(c.value, 10.0);
+    assert_eq!(c.grad, 0.0);
+    assert_eq!(c.requires_grad, true);
+    assert_eq!(c.parent_nodes, vec![(&a, b.value), (&b, a.value)]);
+}
